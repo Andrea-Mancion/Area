437a993b598e0e8ce43ab61e50b142460e8dec22
tristanepitech <tristan.bros@epitech.eu>
2023-09-25 04:55:21 -0400

    FEAT (area/) : Start the vue CLI Project and make a Navbar

---
+++ WEB/area/src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <b-navbar toggleable="lg" type="dark" variant="info" class="navbar-expand-lg">
+      <b-navbar-brand href="#" @click="redirectHome">Area</b-navbar-brand>
+      <div class="mr-auto">
+        <router-link to="/login">Login</router-link>
+      </div>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  methods: {
+    redirectHome () {
+      this.$router.push('/');
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ WEB/area/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="Login">
+        <text>Enter yout mail :</text>
+        <b-form-input placeholder="Mail"></b-form-input>
+        <text>Enter yout Password :</text>
+        <b-form-input placeholder="Password"></b-form-input>
+    </div>
+</template>
+
+<script>
+
+</script>
