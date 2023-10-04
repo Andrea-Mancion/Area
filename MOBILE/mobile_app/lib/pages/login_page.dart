7001de5f75867770b2a2b0b91c51c8acc84e5519
Martin BOUCAULT <82270680+Martinouaiouaiouai@users.noreply.github.com>
2023-10-04 16:04:01 +0200

    ADD: Pages

--- MOBILE/mobile_app/lib/pages/login_page.dart
+++ MOBILE/mobile_app/lib/pages/login_page.dart
@@ -17,47 +17,77 @@ class LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children:[
-            const SizedBox(height: 100),
-            Image.asset("assets/images/logo.png",
-              width: 250,
-              height: 250,
+            const SizedBox(height: 80),
+            Flexible(
+              flex: 3,
+              child: Image.asset("assets/images/logo.png",
+                width: 400,
+                height: 400,
+                fit: BoxFit.contain,
+              )
             ),
-            const SizedBox(height: 50),
-            const Text("Log in",
-              style:TextStyle(
-                fontSize: 20
+            const SizedBox(height: 80),
+            const Flexible(
+              child: Text("Log in",
+                style:TextStyle(
+                  fontSize: 20
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 10),
-            const SizedBox(
-              width: 330,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
+            const Flexible(
+              flex: 1,
+              child: SizedBox(
+                width: 330,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                  keyboardType: TextInputType.text,
                 ),
-                keyboardType: TextInputType.text,
               ),
             ),
             const SizedBox(height: 20),
-            SizedBox(
-              width: 330,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: _passwordVisible ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
+            Flexible(
+              child: SizedBox(
+                width: 330,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    labelText: 'Password',
+                    suffixIcon: Flexible(
+                      child: IconButton(
+                        icon: _passwordVisible ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                    )
+                  ),
+                  obscureText: !_passwordVisible,
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Flexible(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                child: const Text("Connexion",
+                  style :TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 255, 0, 0)
                   )
                 ),
-                obscureText: !_passwordVisible,
-                keyboardType: TextInputType.text,
               ),
             ),
             const SizedBox(height: 20),
@@ -68,13 +98,25 @@ class LoginPageState extends State<LoginPage> {
                   MaterialPageRoute(builder: (context) => const HomePage()),
                 );
               },
-              child: const Text("Connexion",
-                style :TextStyle(
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 255, 0, 0)
-                )
+              child: SizedBox(
+                width: 200,
+                child: Row(
+                  children: [
+                    Image.asset("assets/images/google.png",
+                      width: 30,
+                      height: 30,
+                    ),
+                    const SizedBox(width: 10),
+                     const Text("Continue with Google",
+                      style :TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 0, 0, 0)
+                      )
+                    ),
+                  ],
+                ),
               ),
-            )
+            ),
           ],
         ),
       )
