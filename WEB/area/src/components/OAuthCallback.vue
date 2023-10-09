cdd514f751ebdc9c835a831318ceacf36ba1e45b
tristanepitech <tristan.bros@epitech.eu>
2023-10-09 11:42:25 -0400

    FEAT(area/): try to create action/reaction with api

---
+++ WEB/area/src/components/OAuthCallback.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Redirection après l'autorisation OAuth2...</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    // Récupérez le code d'autorisation de l'URL
+    const code = this.$route.query.code;
+    // Utilisez Axios pour échanger le code d'autorisation contre un jeton d'accès
+    axios
+      .post("https://accounts.spotify.com/api/token", null, {
+        params: {
+          code: code,
+          client_id: "fdbe5e5dbe5c42b680efb3ab1d3574af",
+          client_secret: "8f5b9b576da44c32bf53b1fa53786b8f",
+          redirect_uri: "http://localhost:8080/oauth-callback",
+          grant_type: "authorization_code",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((response) => {
+        const accessToken = response.data.access_token;
+        this.$store.commit("setSpotifyToken", accessToken);
+        this.$router.push("/");
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'échange du code d'autorisation : ", error);
+      });
+  },
+};
+</script>
---
+++ WEB/area/src/views/ActionApplicationListView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Welcome to the Action Application list view !</p>
+        <ActionApplicationComp ImageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Spotify_App_Logo.svg/2048px-Spotify_App_Logo.svg.png"></ActionApplicationComp>
+    </div>
+</template>
+
+<script>
+import ActionApplicationComp from "../components/ActionApplication.vue"
+
+export default {
+    name: "actionApplicationList",
+    components: {
+        ActionApplicationComp,
+    }
+}
+
+</script>
