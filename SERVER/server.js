3d7df0e2551f3f3d39b80ce5ee83a1e759a85b61
mael <mael.gouyet@epitech.eu>
2023-10-10 18:54:04 +0200

    ADD: correct the way to take the information

--- SERVER/server.js
+++ SERVER/server.js
@@ -20,33 +20,7 @@ app.set('views', path.join(__dirname, 'views')); // Dossier où se trouvent les 
 // Middleware pour gérer les données POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 let area = [];
-
-// app.get("/createDatabase", (req, res) => {
-
-//     let databaseName = "gfg_db";
-
-//     let createQuery = `CREATE DATABASE ${databaseName}`;
-
-//     // use the query to create a Database.
-//     database.query(createQuery, (err) => {
-//         if(err) throw err;
-
-//         console.log("Database Created Successfully !");
-
-//         let useQuery = `USE ${databaseName}`;
-//         database.query(useQuery, (error) => {
-//             if(error) throw error;
-
-//             console.log("Using Database");
-
-//             return res.send(
-// `Created and Using ${databaseName} Database`);
-//         })
-//     });
-// });
-
 // Route pour l'inscription (register)
 app.get('/register', (req, res) => {
   res.render('register'); // Utilisez res.render pour afficher la page EJS (assurez-vous que 'login.ejs' existe dans le dossier 'views')
@@ -192,6 +166,11 @@ app.get('/create_action', (req, res) => {
   };
 
   area.push(newAreaObject);
+  // spotifyVariables.push(newAreaObject);
+  console.log(spotifyVariables.nbTrack);
+  addNewVariables();
+  setInterval(() => callAction(newAreaObject, nbreact), 3000);
+  nbreact++;
   /*
     pool.connect()
       .then(client => {
