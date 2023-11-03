bb5bde30d3f918a485846fbda55c7252e5527314
mael <mael.gouyet@epitech.eu>
2023-11-03 17:15:41 +0100

    FIX: correct the way to get the args

--- SERVER/dailymotion/reaction.js
+++ SERVER/dailymotion/reaction.js
@@ -9,10 +9,10 @@ async function callReactiondailymotion(area)
 
 async function createPlaylist(area)
 {
-    let access_token = area.access_token;
-    let playlist_name = area.playlist_name;
-    let playlist_description = area.playlist_description;
-    let playlist_privacy = area.playlist_privacy;
+    let access_token = area.reaction_access_token;
+    let playlist_name = area.reaction_Param.playlist_name;
+    let playlist_description = area.reaction_Param.playlist_description;
+    let playlist_privacy = area.reaction_Param.playlist_privacy;
 
     fetch('https://api.dailymotion.com/me/playlists?access_token=' + access_token + '&name=' + playlist_name + '&description=' + playlist_description + '&privacy=' + playlist_privacy, {
         method: 'POST',
